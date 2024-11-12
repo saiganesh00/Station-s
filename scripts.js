66e86929b6a6700b39fd4765b5137782a40ae64a
Shanmukh Maddimsetti <140135125+saiganesh00@users.noreply.github.com>
2024-11-12 17:51:59 +0530

    Day-1

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('btn');
+const text = document.getElementById('displayText');
+
+button.addEventListener('click', function() 
+{text.style.display = 'block'; })
